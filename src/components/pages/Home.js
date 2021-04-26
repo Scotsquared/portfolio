b17d82add3448d4aa2a9a1e158975305bc38e5df
Scotsquared <didipoopidid98@gmail.com>
2021-04-26 13:23:44 -0500

    removed unessacary labels for social media and readjusted the logos accordingly to fit on the footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,29 +14,6 @@ function Home() {
                         <div class='footer-link-items'>
                             <div class='footer-link-items'>
                                 <h2>Social Media</h2>
-
-                                <a
-                                    target="_blank"
-                                    href={"https://github.com/Scotsquared"}>
-                                    Github
-                                </a>
-                                <a
-                                    target="_blank"
-                                    href={"https://www.instagram.com/scot__dot/"}>
-                                    Instagram
-                                </a>
-                                <a
-                                    target="_blank"
-                                    href={"https://www.facebook.com/scotland.bullard/"}>
-                                    Facebook
-                                </a>
-                                <a to='/'>
-                                    <a
-                                        target="_blank"
-                                        href={"https://twitter.com/Scot__dot"}>
-                                        Twitter
-                                    </a>
-                                </a>
                             </div>
                         </div>
                         <div class='social-media-wrap'>
